f222fcf783fff29548f45b84a5406a463945d3de
RomanAchilov <dakaktak95@gmail.com>
2021-03-31 14:59:04 +0300

    feat: implement footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: 'blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/Drill4J',
           label: 'GitHub',
@@ -34,53 +34,102 @@ module.exports = {
       ],
     },
     footer: {
-      style: 'dark',
+      copyright: `
+          <span class="footer__copyright__info">
+            Sponsored by
+            <a href="https://www.epam-group.ru/" target="_blank" rel="noreferrer noopener">
+              <img src="img/epam-footer-logo.svg" alt="Epam" />
+            </a>
+            Licensed under Apache v2.0
+          </span>
+          <div class="footer__copyright__social-links">
+            <a 
+              class="footer__copyright__social-links__git-hub"
+              href="https://github.com/Drill4J" target="_blank"
+              rel="noreferrer noopener"
+            ></a>
+            <a 
+              class="footer__copyright__social-links__telegram"
+              href="https://t.me/drill4j" 
+              target="_blank" rel="noreferrer noopener"
+            ></a>
+            <a 
+              class="footer__copyright__social-links__twiter"
+              href="#" target="_blank" rel="noreferrer noopener"
+            ></a>
+            <a 
+              class="footer__copyright__social-links__you-tube"
+              href="https://www.youtube.com/channel/UCJtegUnUHr0bO6icF1CYjKw/featured"
+              target="_blank" rel="noreferrer noopener"
+            ></a>
+          </div>
+      `,
       links: [
         {
-          title: 'Docs',
           items: [
             {
-              label: 'Style Guide',
+              html: `
+              <div class="footer__item__logo">
+                <a href="https://github.com/Drill4J" target="_blank" rel="noreferrer noopener">
+                  <img src="img/logo-section-footer.svg" alt="drill4j github" />
+                </a>
+              </div>
+                `,
+            },
+          ],
+        },
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: 'Features',
               to: 'docs/',
             },
             {
-              label: 'Second Doc',
+              label: 'Plugins',
+              to: 'docs/doc2/',
+            },
+            {
+              label: 'FAQ',
               to: 'docs/doc2/',
             },
           ],
         },
         {
-          title: 'Community',
+          title: 'Installation',
           items: [
             {
-              label: 'Stack Overflow',
+              label: 'Java Agent',
               href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             },
             {
-              label: 'Discord',
+              label: 'JS Agent',
               href: 'https://discordapp.com/invite/docusaurus',
             },
             {
-              label: 'Twitter',
+              label: 'Auto tests Agent',
+              href: 'https://twitter.com/docusaurus',
+            },
+            {
+              label: 'Chrome Browser Extension',
               href: 'https://twitter.com/docusaurus',
             },
           ],
         },
         {
-          title: 'More',
+          title: 'Drill4j',
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: 'About',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: 'Contacts us',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
