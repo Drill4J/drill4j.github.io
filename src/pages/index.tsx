70f7cc8a4c34f614375187b982265b7d2ab0cfa1
RomanAchilov <dakaktak95@gmail.com>
2021-04-05 18:08:28 +0300

    fix: update positions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,34 +28,34 @@ function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <div className={styles.parallax}>
-        <div className="container">
-          <div className="pt-52 space-y-4">
-            <h1 className="text-center">
-              Minimize Your Regression <br />
-              Suite with Drill4J
-            </h1>
-            <p className="text-center text-monochrome-default">
-              Drill4J is “feature-on-demand” tool for real-time application profiling that does not <br />
-              affect codebase. It provides the ability to make white box functional testing, via <br />
-              access to application instructions and memory.
-            </p>
-          </div>
-          <RellaxWrapper speed={-1} className={styles.cube1} />
-          <RellaxWrapper speed={-2} className={styles.cube2} />
-          <RellaxWrapper speed={-2} className={styles.cube3} />
-          <RellaxWrapper speed={-1} className={styles.cube4} />
-          <RellaxWrapper speed={-3} className={styles.cube5} />
-          <RellaxWrapper speed={1} className={styles.cube6} />
-          <RellaxWrapper speed={-2} className={styles.cube7} />
-          <RellaxWrapper speed={-1} className={styles.cube8} />
-          <RellaxWrapper speed={-1} className={styles.cube9} />
-          <RellaxWrapper speed={-2} className={styles.cube10} />
-          <RellaxWrapper speed={-4} className={styles.cube11} />
-          <RellaxWrapper speed={-4} className={styles.cube12} />
-        </div>
-      </div>
       <main className={styles.mainPageWrapper}>
+        <div className={styles.parallax}>
+          <div className="relative container h-full">
+            <div className="pt-52 space-y-4">
+              <h1 className="text-center">
+                Minimize Your Regression <br />
+                Suite with Drill4J
+              </h1>
+              <p className="text-center text-monochrome-default">
+                Drill4J is “feature-on-demand” tool for real-time application profiling that does not <br />
+                affect codebase. It provides the ability to make white box functional testing, via <br />
+                access to application instructions and memory.
+              </p>
+            </div>
+            <RellaxWrapper speed={3} className={`${styles.cube1} hidden md:block`} />
+            <RellaxWrapper speed={4} className={`${styles.cube2} hidden md:block`} />
+            <RellaxWrapper speed={4} className={`${styles.cube3} hidden lg:block`} />
+            <RellaxWrapper speed={3} className={`${styles.cube4} hidden md:block`} />
+            <RellaxWrapper speed={5} className={`${styles.cube5} hidden lg:block`} />
+            <RellaxWrapper speed={0} className={`${styles.cube6} hidden md:block`} />
+            <RellaxWrapper speed={4} className={`${styles.cube7} hidden lg:block`} />
+            <RellaxWrapper speed={4} className={`${styles.cube8} hidden md:block`} />
+            <RellaxWrapper speed={3} className={`${styles.cube9} hidden md:block`} />
+            <RellaxWrapper speed={4} className={`${styles.cube10} hidden lg:block`} />
+            <RellaxWrapper speed={5} className={`${styles.cube11} hidden lg:block`} />
+            <RellaxWrapper speed={3} className={`${styles.cube12} hidden md:block`} />
+          </div>
+        </div>
         <section className="container">
           <Figure>
             <img className="lg:order-1 bg-contain" src={useBaseUrl('img/time-savings.png')} alt="time savings" />
