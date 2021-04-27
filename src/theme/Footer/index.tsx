2e7a7f56140bc95c59380b32ad84ffcede3adec1
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-04-27 18:04:46 +0300

    fix: footer layout

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -23,12 +23,12 @@ const Footer = () => {
   return (
     <footer className="bg-monochrome-light-tint">
       <div className="container">
-        <section className="grid py-16 grid-cols-1 md:grid-cols-3 lg:grid-cols-12 gap-y-12 gap-x-5">
+        <section className="grid py-16 grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-y-12 gap-x-5">
           <a href="https://github.com/Drill4J" target="_blank" rel="noreferrer noopener" className="col-span-full lg:col-span-4">
             <img src={useBaseUrl('img/drill4j-logo-grayscale.svg')} alt="drill4j github" />
           </a>
           {links.map(({ title = '', items }, index) => (
-            <div className={clsx('space-y-3', `lg:col-span-3 lg:col-start-${5 + index * 3}`)}>
+            <div className={clsx('space-y-3', `md:col-span-${index === 2 ? 2 : 3} lg:col-span-3 lg:col-start-${5 + index * 3}`)}>
               <h5>{title}</h5>
               <ul>
                 {items.map(({ label, href }) => (
