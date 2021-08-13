fda585adf981c13420b443282ad5a68fec39ba1a
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-08-13 09:20:53 +0300

    feat: update warning and info block and "how to start" page

--- src/components/java-plus-js-header/java-plus-js-header.tsx
+++ src/components/java-plus-js-header/java-plus-js-header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export const JavaPlusJsHeader = () => (
-  <>
+  <div className="flex flex-col items-center">
     <h3>We're using a RealWorld app examples:</h3>
-    <div className="flex items-center gap-x-6 mb-12 pl-10 text-16 leading-20">
+    <div className="flex gap-x-6 mb-12 pl-10 text-16 leading-20">
       <div className="flex justify-center items-center gap-x-3 leading-20">
-        <img style={{ marginBottom: 0 }} src="/img/spring-logo.svg" alt="spring-logo" />
+        <img style={{ marginBottom: 0 }} src={useBaseUrl('img/spring-logo.svg')} alt="spring-logo" />
         <span className="flex flex-col">Backend:&nbsp;
           <a
             className="leading-20"
@@ -18,10 +19,10 @@ export const JavaPlusJsHeader = () => (
         </span>
       </div>
       <div className="flex justify-center items-center">
-        <img style={{ marginBottom: 0 }} src="/img/plus.svg" alt="plus" />
+        <img style={{ marginBottom: 0 }} src={useBaseUrl('img/plus.svg')} alt="plus" />
       </div>
       <div className="flex justify-center items-center gap-x-3">
-        <img style={{ marginBottom: 0 }} src="/img/angular-logo.svg" alt="angular-logo" />
+        <img style={{ marginBottom: 0 }} src={useBaseUrl('img/angular-logo.svg')} alt="angular-logo" />
         <span className="flex flex-col leading-20">Frontend:&nbsp;
           <a className="leading-20" href="https://github.com/gothinkster/angular-realworld-example-app" target="_blank" rel="noreferrer">
             Angular
@@ -29,5 +30,5 @@ export const JavaPlusJsHeader = () => (
         </span>
       </div>
     </div>
-  </>
+  </div>
 );
