45e97cc4476b2fc937b99ef51f57c986ca2f8612
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-09-09 10:20:29 +0300

    feat: add examples to the sidebar

--- sidebars.js
+++ sidebars.js
@@ -38,10 +38,15 @@ const docs = [
   {
     type: 'category',
     label: 'Releases',
-    items: ['releases/pre-release-0-8-0-alpha', 'releases/release-0-7-0', 'releases/release-0-6-0', 'releases/release-0-5-0', 'releases/release-0-4-0', 'releases/release-0-3-0', 'releases/release-0-2-0'],
+    items: ['releases/pre-release-0-8-0-alpha', 'releases/release-0-7-0', 'releases/release-0-6-0',
+      'releases/release-0-5-0', 'releases/release-0-4-0', 'releases/release-0-3-0', 'releases/release-0-2-0'],
   },
   'supported-frameworks', 'faq',
 ];
+
+if (process.env.NODE_ENV !== 'production') {
+  docs.push('examples');
+}
 
 module.exports = {
   docs,
