4df90df0e0e09e610c667adbea4009fc3e8a2f8c
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-17 09:48:04 +0300

    feat: create component with mermaidJs

---
+++ src/components/mermaid/mermaid.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import mermaid from 'mermaid';
+
+mermaid.initialize({
+  startOnLoad: true
+});
+
+interface Props {
+  chart: string;
+}
+
+export const MermaidComponent = ({ chart }: Props) => {
+  useEffect(() => {
+    mermaid.contentLoaded();
+  }, []);
+  return <div className="mermaid">{chart}</div>;
+};
