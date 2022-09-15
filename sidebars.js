9500d01c9ea702436d34474811d8eaa7b5e57779
RomanDavlyatshin <davlyatshinra@gmail.com>
2022-09-15 18:27:04 +0300

    docs: rename getting-started section to installation

--- sidebars.js
+++ sidebars.js
@@ -1,48 +1,102 @@
 const docs = [
   {
     type: 'category',
-    label: 'Installation',
-    items: ['installation/drill-admin', 'installation/java-agent', 'installation/microservices',
-      'installation/autotest-agent', 'installation/extension'],
+    label: 'Overview',
+    items: ['overview/why-drill4j', 'overview/key-differences'],
   },
   {
     type: 'category',
-    label: 'Configuration',
-    items: ['configuration/launch-parameters'],
-  },
-  {
-    type: 'category',
-    label: 'For Development',
+    label: 'Concepts',
     items: [
       {
-        type: 'category',
-        label: 'Java Agent',
-        items: ['for-development/java-agent/java-coverage-receiving-event-flow', 'for-development/java-agent/java-switch-scope-event-flow'],
+        type: 'doc',
+        label: 'Glossary',
+        id: 'basic-concepts/glossary',
       },
       {
-        type: 'category',
-        label: 'JavaScript Agent',
-        items: ['for-development/java-script-agent/js-agent-admin-be-communication'],
+        type: 'doc',
+        label: 'Microservices Guide',
+        id: 'basic-concepts/microservices',
       },
     ],
   },
   {
     type: 'category',
-    label: 'API',
-    items: ['api/builds-summary'],
+    label: 'Installation',
+    items: [
+      'installation/setup',
+      'installation/setup-java-agent',
+      'installation/setup-javascript-agent',
+      'installation/setup-service-group',
+    ],
   },
-  {
-    type: 'category',
-    label: 'Diagrams',
-    items: ['diagrams/auto-test-agent-diagrams', 'diagrams/testing-diagrams'],
-  },
+  'faq',
   {
     type: 'category',
     label: 'Releases',
-    items: ['releases/pre-release-0-8-0-alpha', 'releases/release-0-7-0', 'releases/release-0-6-0',
-      'releases/release-0-5-0', 'releases/release-0-4-0', 'releases/release-0-3-0', 'releases/release-0-2-0'],
+    items: [
+      'releases/pre-release-0-8-0-alpha',
+      'releases/release-0-7-0',
+      'releases/release-0-6-0',
+      'releases/release-0-5-0',
+      'releases/release-0-4-0',
+      'releases/release-0-3-0',
+      'releases/release-0-2-0',
+    ],
   },
-  'supported-frameworks', 'core-concepts', 'faq'
+  {
+    type: 'category',
+    label: 'Docs Archive (pre 0.8.0)',
+    items: [
+      {
+        type: 'category',
+        label: 'setup',
+        items: [
+          'pre-0.8.0/installation/drill-admin',
+          'pre-0.8.0/installation/java-agent',
+          'pre-0.8.0/installation/microservices',
+          'pre-0.8.0/installation/autotest-agent',
+          'pre-0.8.0/installation/extension',
+        ],
+      },
+      {
+        type: 'category',
+        label: 'Configuration',
+        items: ['pre-0.8.0/configuration/launch-parameters'],
+      },
+      {
+        type: 'category',
+        label: 'For Development',
+        items: [
+          {
+            type: 'category',
+            label: 'Java Agent',
+            items: [
+              'pre-0.8.0/for-development/java-agent/java-coverage-receiving-event-flow',
+              'pre-0.8.0/for-development/java-agent/java-switch-scope-event-flow',
+            ],
+          },
+          {
+            type: 'category',
+            label: 'JavaScript Agent',
+            items: ['pre-0.8.0/for-development/java-script-agent/js-agent-admin-be-communication'],
+          },
+        ],
+      },
+      {
+        type: 'category',
+        label: 'API',
+        items: ['pre-0.8.0/api/builds-summary'],
+      },
+      {
+        type: 'category',
+        label: 'Diagrams',
+        items: ['pre-0.8.0/diagrams/auto-test-agent-diagrams', 'pre-0.8.0/diagrams/testing-diagrams'],
+      },
+      'pre-0.8.0/supported-frameworks',
+      'pre-0.8.0/core-concepts',
+    ],
+  },
 ];
 
 module.exports = {
