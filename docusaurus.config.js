d65cd38e33d46567cd8ad03605c17c521bdba65d
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-08-26 14:53:40 +0400

    chore: formatting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,120 +1,119 @@
-const path = require("path");
-const math = require("remark-math");
-const katex = require("rehype-katex");
+const path = require('path');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 module.exports = {
-  title: "Drill4J",
-  tagline: "The tagline of my site",
-  url: "https://Drill4J.github.io",
-  baseUrl: "/",
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "/img/favicon.ico",
-  organizationName: "Drill4J",
-  projectName: "drill4j.github.io",
+  title: 'Drill4J',
+  tagline: 'The tagline of my site',
+  url: 'https://Drill4J.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: '/img/favicon.ico',
+  organizationName: 'Drill4J',
+  projectName: 'drill4j.github.io',
   themeConfig: {
     algolia: {
-      apiKey: "6893440cddb296d0faf1399850200fc6",
-      indexName: "drill4j"
+      apiKey: '6893440cddb296d0faf1399850200fc6',
+      indexName: 'drill4j',
     },
     colorMode: {
-      defaultMode: "light",
-      disableSwitch: true
+      defaultMode: 'light',
+      disableSwitch: true,
     },
     prism: {
-      theme: require("prism-react-renderer/themes/github"),
-      additionalLanguages: ["java"]
+      theme: require('prism-react-renderer/themes/github'),
+      additionalLanguages: ['java'],
     },
     navbar: {
       items: [
         {
-          to: "docs/overview/why-drill4j",
-          label: "Documentation"
+          to: 'docs/overview/why-drill4j',
+          label: 'Documentation',
         },
         {
-          to: "https://github.com/Drill4J/realworld-java-and-js-coverage/",
-          label: "Install demo"
-        }
-      ]
+          to: 'https://github.com/Drill4J/realworld-java-and-js-coverage/',
+          label: 'Install demo',
+        },
+      ],
     },
     footer: {
       links: [
         {
-          title: "1",
+          title: '1',
           items: [
             {
-              label: "Intro",
-              href: "/docs/overview/why-drill4j"
+              label: 'Intro',
+              href: '/docs/overview/why-drill4j',
             },
             {
-              label: "Getting Started",
-              href: "/docs/installation/setup"
+              label: 'Getting Started',
+              href: '/docs/installation/setup',
             },
             {
-              label: "Java Agent",
-              href: "/docs/installation/setup-java-agent"
-            }
-          ]
+              label: 'Java Agent',
+              href: '/docs/installation/setup-java-agent',
+            },
+          ],
         },
         {
-          title: "2",
+          title: '2',
           items: [
             {
-              label: "Chrome Extension",
-              href: "/docs/installation/setup-browser-extension"
+              label: 'Chrome Extension',
+              href: '/docs/installation/setup-browser-extension',
             },
             {
-              label: "Java + JS example App",
-              href: "https://github.com/Drill4J/realworld-java-and-js-coverage/"
-            }
-          ]
+              label: 'Java + JS example App',
+              href: 'https://github.com/Drill4J/realworld-java-and-js-coverage/',
+            },
+          ],
         },
         {
-          title: "3",
+          title: '3',
           items: [
             {
-              label: "Contact us",
-              href: "https://t.me/drill4j"
-            }
-          ]
-        }
-      ]
-    }
+              label: 'Contact us',
+              href: 'https://t.me/drill4j',
+            },
+          ],
+        },
+      ],
+    },
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           remarkPlugins: [math],
-          rehypePlugins: [katex]
+          rehypePlugins: [katex],
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.scss")
+          customCss: require.resolve('./src/css/custom.scss'),
         },
-      }
-    ]
+      },
+    ],
   ],
   plugins: [
-    "docusaurus-plugin-sass",
+    'docusaurus-plugin-sass',
     [
-      "docusaurus-plugin-module-alias",
+      'docusaurus-plugin-module-alias',
       {
         alias: {
-          "@components": path.resolve(__dirname, "./src/components/index.ts"),
-          "@static": path.resolve(__dirname, "./static")
-        }
-      }
-    ]
+          '@components': path.resolve(__dirname, './src/components/index.ts'),
+          '@static': path.resolve(__dirname, './static'),
+        },
+      },
+    ],
   ],
   stylesheets: [
     {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous"
-    }
-  ]
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
 };
