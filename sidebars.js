fadf0f72a73d236fd4a8ff710eb45db5c885b48c
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-08-30 15:34:15 +0400

    feat: remove old "next" docs + add redirect to latest version

--- sidebars.js
+++ sidebars.js
@@ -2,23 +2,7 @@ const docs = [
   {
     type: 'category',
     label: 'Getting Started',
-    items: [
-      {
-        type: 'doc',
-        id: 'installation/deploy-drill4j-services',
-        label: 'Deploy Drill4J services',
-      },
-      {
-        type: 'doc',
-        id: 'installation/what-is-drill4j',
-        label: 'Java / JVM Agent',
-      },
-      {
-        type: 'doc',
-        id: 'installation/setup-java-autotest-agent',
-        label: 'Java Autotest Agent',
-      },
-    ],
+    items: [],
   },
 ];
 
