d096b81cc06aa78b38a2e5866494a4d1de7857d3
RomanDavlyatshin <davlyatshinra@gmail.com>
2024-09-09 19:57:45 +0400

    feat: add alt and description to image component

--- src/components/image-viewer/image.tsx
+++ src/components/image-viewer/image.tsx
@@ -8,15 +8,27 @@ interface Props {
   alt?: string;
 }
 
+const pStyle = {
+  color: '#a9a9a9',
+  fontSize: '14px',
+  marginBottom: '15px'
+};
+
+const imgStyle = {
+  width: '400px',
+  marginBottom: '10px'
+}
+
 export const Image = ({ src, alt }: Props) => {
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   return (
-    <>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <img
         className="cursor-pointer"
         src={useBaseUrl(src)}
         onClick={() => setIsViewerOpen(true)}
         alt={alt}
+        style={imgStyle}
       />
       {isViewerOpen && (
         <Lightbox
@@ -26,6 +38,11 @@ export const Image = ({ src, alt }: Props) => {
           imagePadding={48}
         />
       )}
-    </>
+      <p
+        style={pStyle}
+      >
+        {alt}
+      </p>
+    </div>
   );
 };
