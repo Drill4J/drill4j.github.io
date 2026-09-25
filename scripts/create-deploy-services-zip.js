f36c70ee4ecb6118a6bad59f9971e0ddeab05183
RomanDavlyatshin <davlyatshinra@gmail.com>
2026-09-25 16:37:55 +0400

    fix: drill4j services deployment zip

--- scripts/create-deploy-services-zip.js
+++ scripts/create-deploy-services-zip.js
@@ -2,29 +2,44 @@ const fs = require("fs");
 const path = require("path");
 const AdmZip = require("adm-zip");
 
-const sourceDir = path.resolve(__dirname, "..", "static", "files", "0.10.0", "deploy-drill4j-services");
-const outputZip = path.join(sourceDir, "deploy-drill4j-services.zip");
+const filesRoot = path.resolve(__dirname, "..", "static", "files");
 
-const dockerComposePath = path.join(sourceDir, "docker-compose.yml");
-const envSourcePath = path.join(sourceDir, "drill.env");
-const dbInitPath = path.join(sourceDir, "db-init");
-
-[dockerComposePath, envSourcePath, dbInitPath].forEach((filePath) => {
-  if (!fs.existsSync(filePath)) {
-    throw new Error(`Required file or folder is missing: ${filePath}`);
-  }
-});
-
-if (fs.existsSync(outputZip)) {
-  fs.unlinkSync(outputZip);
+function hasRequiredDeployFiles(sourceDir) {
+  return ["docker-compose.yml", "drill.env", "db-init"].every((name) =>
+    fs.existsSync(path.join(sourceDir, name))
+  );
 }
 
-const zip = new AdmZip();
-zip.addLocalFile(dockerComposePath);
-zip.addLocalFile(envSourcePath, "", ".env");
-zip.addLocalFolder(dbInitPath, "db-init");
-zip.writeZip(outputZip);
+function createDeployServicesZip(sourceDir) {
+  const outputZip = path.join(sourceDir, "deploy-drill4j-services.zip");
+  const dockerComposePath = path.join(sourceDir, "docker-compose.yml");
+  const envSourcePath = path.join(sourceDir, "drill.env");
+  const dbInitPath = path.join(sourceDir, "db-init");
 
-console.log(`Created ${outputZip}`);
+  if (fs.existsSync(outputZip)) {
+    fs.unlinkSync(outputZip);
+  }
 
+  const zip = new AdmZip();
+  zip.addLocalFile(dockerComposePath);
+  zip.addLocalFile(envSourcePath, "", ".env");
+  zip.addLocalFolder(dbInitPath, "db-init");
+  zip.writeZip(outputZip);
 
+  console.log(`Created ${outputZip}`);
+}
+
+const versions = fs.readdirSync(filesRoot).filter((name) => {
+  const deployDir = path.join(filesRoot, name, "deploy-drill4j-services");
+  return fs.existsSync(deployDir) && hasRequiredDeployFiles(deployDir);
+});
+
+if (versions.length === 0) {
+  throw new Error(
+    "No deploy-drill4j-services folders with required files found under static/files"
+  );
+}
+
+versions.forEach((version) => {
+  createDeployServicesZip(path.join(filesRoot, version, "deploy-drill4j-services"));
+});
